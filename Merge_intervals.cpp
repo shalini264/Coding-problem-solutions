a013c6554164cdf3169e633fbed694becab86be3
shalini sharma <48730336+shalini264@users.noreply.github.com>
2021-07-14 08:28:24 +0530

    Update Merge_intervals.cpp

--- Merge_intervals.cpp
+++ Merge_intervals.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    int overlapp(int p,int q)
-    {
-        if(q<=p)
-            return 1;
-        return 0;
-    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        ios_base::sync_with_stdio;
-        cin.tie(NULL);
-        cout.tie(NULL);
         sort(intervals.begin(),intervals.end());
-        vector<int>visited(intervals.size(),0);
-         for(int i=0;i<intervals.size()-1;i++)
-         {  
-              if(visited[i]==1||visited[i+1]==1)
-                   continue;
-                 if(overlapp(intervals[i][1],intervals[i+1][0]))
-                 {visited[i]=1;
-                     int p=max(intervals[i][1],intervals[i+1][1]);
-                     intervals[i+1][0]=intervals[i][0];
-                     intervals[i+1][1]=p;
-                 }
-             
-         }
         vector<vector<int>>ans;
         for(int i=0;i<intervals.size();i++)
-        {   if(visited[i]==0)
         {
-            ans.push_back(intervals[i]);}
+            if(ans.size()==0)
+            {
+                ans.push_back(intervals[i]);
+            }
+            else
+            {
+                int u=ans.size();
+                if(intervals[i][0]>=ans[u-1][0]&&intervals[i][0]<=ans[u-1][1])
+                {
+                    ans[u-1][0]=min(intervals[i][0],ans[u-1][0]);
+                    ans[u-1][1]=max(intervals[i][1],ans[u-1][1]);
+                }
+                else
+                    ans.push_back(intervals[i]);
+            }
         }
         return ans;
     }
