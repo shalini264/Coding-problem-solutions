f56ccad21555561f2fe4724bddc8cdd76c2eee87
shalini sharma <48730336+shalini264@users.noreply.github.com>
2021-07-17 09:35:05 +0530

    Create rotate_by_k.cpp

---
+++ rotate_by_k.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>ans(n,0);
+        for(int i=0;i<n;i++)
+        {
+            ans[(i+k)%n]=nums[i];
+        }
+        nums=ans;
+    }
+};
